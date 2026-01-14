6ab91e76199632415e17dc3091a47345258d2cae
miyyer <miyyer@localhost.localdomain>
2026-01-14 15:10:37 -0500

    Basic Configuration React+Babel+Webpack

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import Footer from './Footer.jsx';
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to My React App 2</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development', // Importante para que el servidor sea rápido
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    clean: true, // Limpia la carpeta dist en cada build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // Tu archivo fuente
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    hot: true, // Habilita Hot Module Replacement
+    port: 3000, // Puedes cambiar el puerto aquí
+    open: true, // Abre el navegador automáticamente
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
